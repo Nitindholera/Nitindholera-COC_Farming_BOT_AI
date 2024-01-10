5d4fe6f7f3031e14c23ab5e928b82a60909a1442
Nitindholera <nitin716sharma@gmail.com>
2024-01-10 23:12:44 +0530

    fixed bug

--- main.py
+++ main.py
@@ -11,20 +11,22 @@ class coc_bot:
         self.confirm_surrender_coordinates = (1140, 670)
         self.return_home_coordinates = (960, 900)
 
-        self.mid_coordinates = (1920/2, 1080/2)
-        self.cart_coordinates = (1220, 70)
+        self.mid_coordinates = (960, 540)
+        self.cart_coordinates = (1390, 33)
         self.cart_collect_coordinates = (1422, 912)
 
     def attack(self):
-        for _ in range(8):
+        for _ in range(1):
             pyautogui.click(self.attack_coordinates)
             time.sleep(0.2)
             pyautogui.click(self.find_now_coordinates)
-            time.sleep(5)
+            time.sleep(6)
             pyautogui.click(self.troop_coordinates)
             time.sleep(.2)
-            pyautogui.click(self.deploy_coordinates)
-            time.sleep(.2)
+            for _ in range(6):
+                pyautogui.click(self.deploy_coordinates)
+                time.sleep(.2)
+            time.sleep(40)
             pyautogui.click(self.surrender_coordinates)
             time.sleep(.2)
             pyautogui.click(self.confirm_surrender_coordinates)
@@ -33,9 +35,10 @@ class coc_bot:
             time.sleep(3)
         
     def collect(self):
-        for _ in range(5):
-            pyautogui.moveTo(self.mid_coordinates)
-            pyautogui.scroll(-500)
+        # for _ in range(5):
+        #     pyautogui.moveTo(960, 500)
+        #     pyautogui.scroll(-500)
+        #     time.sleep(0.2)
         
         time.sleep(.2)
         pyautogui.click(self.cart_coordinates)
@@ -45,10 +48,13 @@ class coc_bot:
         pyautogui.click(self.attack_coordinates)
         time.sleep(.2)
 
+        
+
 
 
 if __name__ == '__main__':
     time.sleep(2)
+    # pyautogui.moveTo(960, 540)
     bot = coc_bot()
     while True:
         bot.attack()
